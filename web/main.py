16faed70cec3f4c64f2e81a2968fe0957b197dcd
lzjqsdd <lzj7179@163.com>
2016-05-08 23:46:31 +0800

    修复搜索框内查询

--- web/main.py
+++ web/main.py
@@ -6,7 +6,8 @@ from ml.Search import Search
 render = web.template.render('templates/')
 
 urls=(
-	'/(.*)','index'
+	"/","index",
+	"/news","news"
 )
 
 app = web.application(urls,globals())
@@ -14,17 +15,23 @@ app = web.application(urls,globals())
 class index:
 	def __init__(self):
 		self.se = Search()
-	def GET(self,searchword):
+	def GET(self):
+		searchword = web.input().searchword
 		newslist=list()
 		if searchword:
 			newslist = self.se.QueryPhrase(searchword,False)
 		return render.header(searchword,newslist)
-	def POST(self):
-		searchword = web.input(searchword=[])
+	
+class news:
+	def __init__(self):
+		self.se = Search()
+	def GET(self):
+		searchword = web.input().searchword
+		newslist=list()
 		if searchword:
 			newslist = self.se.QueryPhrase(searchword,False)
 		return render.header(searchword,newslist)
-	
+
 if __name__ == "__main__":
 	app = web.application(urls,globals())
 	app.run()
